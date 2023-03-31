905e0a4c38837481b20e41903d9ca147b18086ff
Harsh <harshpachani@gmail.com>
2023-03-31 22:50:15 +0530

    RealTime Database

--- CRUD/3.firebaseRealtimeDBDemo.py
+++ CRUD/3.firebaseRealtimeDBDemo.py
@@ -1,31 +1,6 @@
-# import pyrebase
-
-# firebaseConfig = {
-#     "apiKey": "AIzaSyDSLcLu6GSxece0XVRLZKIvwQk9qx1t4BU",
-#     "authDomain": "authdemo-91b1a.firebaseapp.com",
-#     "projectId": "authdemo-91b1a",
-#     "databaseURL": "https://authdemo-91b1a-default-rtdb.firebaseio.com",
-#     "storageBucket": "authdemo-91b1a.appspot.com",
-#     "messagingSenderId": "769208992989",
-#     "appId": "1:769208992989:web:7e40535e9201a0c00f4734",
-#     "measurementId": "G-56WFZTBLVL"
-# }
-
-# firebase = pyrebase.initialize_app(firebaseConfig)
-
-# db = firebase.database()
-
-# #push data
-# data = {
-#     "Name": "Harsh", "Age": 20, "Address": ["Ahmedabad", "Junagadh", "Gandhinagar"]
-# }
-
-# db.Push(data)
-
 import pyrebase
 
-#Initialize Firebase
-firebaseConfig={
+firebaseConfig = {
     "apiKey": "AIzaSyDSLcLu6GSxece0XVRLZKIvwQk9qx1t4BU",
     "authDomain": "authdemo-91b1a.firebaseapp.com",
     "projectId": "authdemo-91b1a",
@@ -36,10 +11,17 @@ firebaseConfig={
     "measurementId": "G-56WFZTBLVL"
 }
 
-firebase=pyrebase.initialize_app(firebaseConfig)
+firebase = pyrebase.initialize_app(firebaseConfig)
 
-db=firebase.database()
+db = firebase.database()
 
-#Push Data
-data={"age":20, "address":["new york", "los angeles"]}
+#push data
+data = {
+    "Name": "Harsh", "Age": 20, "Address": ["Ahmedabad", "Junagadh", "Gandhinagar"]
+}
+
 print(db.push(data)) #unique key is generated
+
+#To Change the Data.
+# Using set() overwrites data at the specified location, including any child nodes.
+# print(db.set(data)) #unique key is generated

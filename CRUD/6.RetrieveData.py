3713a0e958c2ff07874db643efa9d12d9e3417c5
Harsh <harshpachani@gmail.com>
2023-04-01 16:38:36 +0530

    Retrieve Data using shallow() and get() method

--- CRUD/6.RetrieveData.py
+++ CRUD/6.RetrieveData.py
@@ -11,11 +11,27 @@ firebaseConfig = {
     "measurementId": "G-56WFZTBLVL"
 }
 
+firebaseConfig2 = {
+    "apiKey": "AIzaSyCzneUDuxGW36MViwUQf2gnT0w7XIyzi1c",
+    "authDomain": "login-8a1a8.firebaseapp.com",
+    "databaseURL": "https://login-8a1a8-default-rtdb.firebaseio.com",
+    "projectId": "login-8a1a8",
+    "storageBucket": "login-8a1a8.appspot.com",
+    "messagingSenderId": "414258770200",
+    "appId": "1:414258770200:web:4faf9a519d3cc54cf42e6d",
+    "measurementId": "G-H2GCMN40BS"
+}
+
 firebase = pyrebase.initialize_app(firebaseConfig)
+firebase2 = pyrebase.initialize_app(firebaseConfig2)
 
 db = firebase.database()
+db2 = firebase2.database()
+
 
 """
+#Created data.
+
 data1 = {"address": "Los Angeles", "age": 20, "firstname": "Jane", "lastname": "Doe"}
 data2 = {"address": "New York", "age": 30, "firstname": "John", "lastname": "Smith"}
 data3 = {"age": 32, "firstname": "Marc", "lastname": "Johnson"}
@@ -27,3 +43,20 @@ db.child("RetrieveData").child("users").push(data3)
 db.child("RetrieveData").child("users").push(data4)
 """
 
+#Retrieve Data
+users = db.child("RetrieveData").child("users").get()
+# print(users.val())
+
+# users2 = db2.get()
+# print(users2.val())
+
+#Iterate over keys
+print("\n\t\t\tIterate Over the keys")
+for user in users.each():
+    print("key: ", user.val())
+    print("Values: ", user.val())
+    print()
+
+#You can also use shallow method to get keys.
+users = db.child("RetrieveData").child("users").shallow().get()
+print("\n\t\t\t\tUsing Shallow() method\n", users.val())

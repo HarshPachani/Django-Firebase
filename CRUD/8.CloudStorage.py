5cfccb74e16ae7476bc32007fc471d56a077d9be
Harsh <harshpachani@gmail.com>
2023-04-01 17:56:57 +0530

    Initial commit

---
+++ CRUD/8.CloudStorage.py
@@ -0,0 +1,23 @@
+import pyrebase
+
+firebaseConfig = {
+    "apiKey": "AIzaSyDmmJcWxCFnAzgBdDcNTJSCRlWvGIU6414",
+    "authDomain": "realtimedbstoragedemo-2c0fb.firebaseapp.com",
+    "projectId": "realtimedbstoragedemo-2c0fb",
+    "storageBucket": "realtimedbstoragedemo-2c0fb.appspot.com",
+    "messagingSenderId": "704263168946",
+    "appId": "1:704263168946:web:47cd2accbeeeaa24813aab",
+    "measurementId": "G-9TTEEPCR3G"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+
+database = firebase.database()
+storage = firebase.storage()
+
+name = input("Enter Name: ")
+age = input("Enter Age: ")
+profession = input("Enter Profession: ")
+
+filename = input("Enter the name of file you want to upload to storage: ")
+cloudfilename = input("Enter the name of the file in cloud: ")
